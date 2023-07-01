4a43f7fca26e61019c5c0df17fcf2b9f55de4d7e
Cesar Garcia <cyberpgp@hotmail.com>
2023-07-01 18:29:18 -0500

    se agrego la funcionalidad de sumar los precios del carrito y nuevos estilos

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -6,11 +6,11 @@ import CartList from '@/components/CartList'
 import { GiShoppingCart } from 'react-icons/gi'
 
 const Cart = () => {
+    
 
-    const [cartItem, setCartItem] = useRecoilState(cartState);    
-    const totalPrice = () => {
+    const [cartItem, setCartItem] = useRecoilState(cartState);     
+    const totalPrice = () => {        
         let total = 0
-        
         cartItem.forEach(item => total += (item.price * item.quantity))
         return total
     }
@@ -26,11 +26,13 @@ const Cart = () => {
                     <h1 className='text-center text-4xl mt-32 font-normal'>Carro Vacio</h1>
                     <GiShoppingCart className='mx-auto h-[12vh] w-[12vw]' />
                 </div> 
-                : cartItem.map(item => <CartList key={item} data={item} />)}
+                : cartItem.map(item => <CartList key={item.id} data={item} />)}
 
                 {cartItem.length > 0 && (<div>
-                    <h2>Total: S/{totalPrice()}</h2>
-                    <button onClick={createCheckoutSession}>CheckOut</button>
+                    <div className='flex flex-col lg:w-[20vw] mx-auto p-10 gap-5 text-center items-center rounded-2xl'>
+                        <h2 className='text-[3vh] font-light'>Total: S/{totalPrice()}</h2>
+                        <button className='bg-[#50C2D8] text-white py-4 px-10 mt-4 block mx-auto hover:bg-white hover:text-[#50C2D8] rounded-2xl border-[#50C2D8] border-2 ease-in-out duration-300 ' onClick={createCheckoutSession}>Pagar</button>
+                    </div>
                 </div>)}
                 
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Maps from '@/components/Maps';
 import Product from '@/components/Product';
 import data from '@/app/data/data.json';
+import Footer from '@/components/Footer';
 
 import React from 'react';
 
@@ -8,12 +9,13 @@ export default function Home() {
   return (
     <main className="flex w-full flex-col items-center justify-between ">
       <section className='container w-[100vw] mt-5'>
-        <h1 className='text-4xl mt-4 text-center'>Nuestros Productos</h1>
+        <h1 className='text-4xl mt-4 text-center font-light'>Nuestros Productos</h1>
         <div className='mt-4 grid grid-cols-1 md:grid-cols-3 gap-4'>
           {data.map(product => <Product product={product} />)}
         </div>
       </section>
       <Maps />
+      <Footer/>
     </main>
   )
 }
